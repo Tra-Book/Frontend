4b0ecb8b0cd4f8bf5110294edd53709bd864d333
Ji__ho <cale.jiho@gmail.com>
2024-08-20 21:36:15 +0900

    feat: font & color added
    tailwindcss variable added

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -1,9 +1,8 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { pretendard } from '@/public/fonts/font'
 
 export const metadata: Metadata = {
   title: 'TraBook',
@@ -13,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={pretendard.className}>{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
+      colors: {
+        tbPrimary: {
+          DEFAULT: '#FFD25E',
+          foreground: 'rgb(var(--color-primary) / <alpha-value>)',
+        },
+        tbSecondary: {
+          DEFAULT: '#FFEFCA',
+          foreground: 'rgb(var(--color-secondary) / <alpha-value>)',
+        },
+        tbGreen: {
+          DEFAULT: '#CCF39A',
+          foreground: 'rgb(var(--color-popup) / <alpha-value>)',
+        },
+        tbBlue: {
+          DEFAULT: '#0277FF',
+          foreground: 'rgb(var(--color-link) / <alpha-value>)',
+        },
+        tbGray: {
+          DEFAULT: '#A0A0A0',
+          foreground: 'rgb(var(--color-placeholder) / <alpha-value>)',
+        },
+        tbRed: {
+          DEFAULT: '#F45858',
+          foreground: 'rgb(var(--color-invalid) / <alpha-value>)',
+        },
       },
     },
   },
