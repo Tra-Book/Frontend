2c1f89f55a59092930a2caa259701b602d189f44
Ji__ho <cale.jiho@gmail.com>
2024-08-31 17:23:45 +0900

    chore: reset backend conenction

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Todo: 연결 풀기
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
-  //     },
-  //   ]
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
+      },
+    ]
+  },
 }
 
 export default nextConfig
