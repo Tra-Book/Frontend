ddefa4735d76313837107fd68afc90522ccb60c4
신진건 <sjg729729@gmail.com>
2024-09-08 16:55:10 +0900

    feat: 비밀번호 변경 페이지 완성

--- app/(route)/(header)/main/password/page.tsx
+++ app/(route)/(header)/main/password/page.tsx
@@ -1,9 +1,202 @@
-import React, { ReactNode } from 'react'
+'use client'
+
+import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+import React, { ChangeEventHandler, Dispatch, ReactNode, SetStateAction, useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { BACKEND_ROUTES, ROUTES } from '@/lib/constants/routes'
+import LucideIcon from '@/lib/icons/LucideIcon'
+import { cn } from '@/lib/utils/cn'
+
+type PasswordState = {
+  password: string
+  isShow: boolean
+}
 
 interface ChangePasswordPageProps {}
 
+function isValidPassword(password: string) {
+  const hasLetter = /[a-zA-Z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+  const length = password.trim().length >= 10 ? true : false
+
+  return hasLetter && hasNumber && hasSpecialChar && length
+}
+
 const ChangePasswordPage = ({}: ChangePasswordPageProps): ReactNode => {
-  return <div className='w-full bg-white'>ChangePasswordPage</div>
+  const [currentPw, setCurrentPw] = useState<PasswordState>({ password: '', isShow: false })
+  const [newPw, setNewPw] = useState<PasswordState>({ password: '', isShow: false })
+  const [checkPw, setCheckPw] = useState<PasswordState>({ password: '', isShow: false })
+
+  const [isValid, setIsValid] = useState<boolean>(true)
+  const [isSame, setIsSame] = useState<boolean>(true)
+
+  const session: any = useSession()
+
+  const onChangeNewPw: ChangeEventHandler<HTMLInputElement> = e => {
+    setNewPw(prev => ({ ...prev, password: e.target.value }))
+
+    if (isValidPassword(newPw.password)) setIsValid(true)
+  }
+
+  const onBlurNewPw: ChangeEventHandler<HTMLInputElement> = e => {
+    if (isValidPassword(newPw.password)) setIsValid(true)
+    else setIsValid(false)
+  }
+
+  const onChangeCheckPw: ChangeEventHandler<HTMLInputElement> = e => {
+    setCheckPw(prev => ({ ...prev, password: e.target.value }))
+
+    if (newPw.password === e.target.value) setIsSame(true)
+    else setIsSame(false)
+  }
+
+  const onClickPwChange = async () => {
+    if (newPw.password.trim() === '') {
+      alert('새 비밀번호를 입력해주세요.')
+      return
+    }
+    if (!isValid) {
+      alert('새 비밀번호 형식이 올바르지 않습니다.')
+      return
+    }
+    if (!isSame) {
+      alert('비밀번호 확인을 다시 입력해주세요.')
+      return
+    }
+
+    try {
+      const res = await fetch(`/server/${BACKEND_ROUTES.AUTH.UPDATE_PASSWORD.url}`, {
+        method: BACKEND_ROUTES.AUTH.UPDATE_PASSWORD.method,
+        headers: {
+          Authorization: session.data.accessToken,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          password: currentPw.password,
+          newPassword: newPw.password,
+        }),
+        credentials: 'include',
+      })
+
+      const status = res.status
+      const data = await res.json()
+      console.log(data)
+
+      switch (status) {
+        case 200:
+          alert('비밀번호 변경이 완료되었습니다.')
+          return
+        case 400:
+          alert('현재 비밀번호가 다릅니다.')
+          break
+        default:
+          alert('다시 시도해주세요.')
+          break
+      }
+    } catch (error) {
+      alert('비밀번호 변경이 실패하였습니다.')
+    }
+  }
+
+  const renderEye = (pw: PasswordState, setPw: Dispatch<SetStateAction<PasswordState>>) => {
+    return (
+      <LucideIcon
+        name={!pw.isShow ? 'EyeOff' : 'Eye'}
+        onClick={() => setPw(prev => ({ ...prev, isShow: !pw.isShow }))}
+        className='absolute right-2 h-full opacity-40'
+        size={24}
+      />
+    )
+  }
+
+  return (
+    <div className='flex w-full flex-col bg-white px-10'>
+      <div className='flex w-full flex-col items-start justify-end gap-4'>
+        <span className='flex h-[8dvh] min-h-[60px] items-end text-2xl font-semibold xl:text-3xl'>내 정보 관리</span>
+        <span className='border-b border-solid border-black text-sm font-medium xl:text-base'>비밀번호 변경</span>
+      </div>
+
+      <div className='mt-9 flex grow flex-col items-center gap-8'>
+        <div className='flex w-full max-w-[300px] flex-col'>
+          <Label htmlFor='currentPw' className='mb-2 flex text-base'>
+            현재 비밀번호 <span className='text-tbRed'>*</span>
+          </Label>
+          <div className='gap relative flex justify-between gap-2'>
+            <Input
+              onChange={e => setCurrentPw(prev => ({ ...prev, password: e.target.value }))}
+              // onBlur={onBlurPassword}
+              type={currentPw.isShow ? 'text' : 'password'}
+              id='currentPw'
+              placeholder=''
+              className={cn('h-13 bg-tbPlaceholder shadow-tb-shadow', false && 'ring-2 ring-tbRed')}
+            />
+            {renderEye(currentPw, setCurrentPw)}
+          </div>
+        </div>
+
+        <div className='flex w-full max-w-[300px] flex-col'>
+          <Label htmlFor='newPw' className='mb-2 flex text-base'>
+            비밀번호 <span className='text-tbRed'>*</span>
+          </Label>
+          <div className='gap relative flex justify-between gap-2'>
+            <Input
+              onChange={onChangeNewPw}
+              onBlur={onBlurNewPw}
+              type={newPw.isShow ? 'text' : 'password'}
+              id='newPw'
+              placeholder=''
+              className={cn('h-13 bg-tbPlaceholder shadow-tb-shadow', !isValid && 'ring-2 ring-tbRed')}
+            />
+            {renderEye(newPw, setNewPw)}
+          </div>
+          <p className={cn(isValid ? 'invisible text-sm' : 'mt-1 pl-2 text-sm text-tbRed')}>
+            * 올바른 형식이 아닙니다.
+          </p>
+        </div>
+
+        <div className='text-center text-sm text-slate-500'>
+          안전한 비밀번호를 위해
+          <br />
+          영문자, 숫자, 특수문자를 포함해야 합니다.
+        </div>
+
+        <div className='flex w-full max-w-[300px] flex-col'>
+          <Label htmlFor='checkPw' className='mb-2 flex text-base'>
+            비밀번호 확인<span className='text-tbRed'>*</span>
+          </Label>
+          <div className='gap relative flex justify-between gap-2'>
+            <Input
+              onChange={onChangeCheckPw}
+              type={checkPw.isShow ? 'text' : 'password'}
+              id='checkPw'
+              placeholder=''
+              className={cn('h-13 bg-tbPlaceholder shadow-tb-shadow', !isSame && 'ring-2 ring-tbRed')}
+            />
+            {renderEye(checkPw, setCheckPw)}
+          </div>
+          <p className={cn(isSame ? 'invisible text-sm' : 'mt-1 pl-2 text-sm text-tbRed')}>
+            * 비밀번호를 확인해주세요.
+          </p>
+        </div>
+
+        <Button onClick={onClickPwChange} variant='tbPrimary' className='mt-3 h-13 w-full max-w-[300px]'>
+          비밀번호 변경
+        </Button>
+      </div>
+
+      <div className='my-3 flex flex-col text-center text-[#817A7A] md:block'>
+        더 이상 TRABOOK과 함께하고 싶지 않으신가요?
+        <Link href={ROUTES.AUTH.SIGNOUT.url} className='mx-5 text-black underline hover:cursor-pointer'>
+          회원 탈퇴
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default ChangePasswordPage
