912adc6da752b5af252bccac3ae8e70e3ffc8563
신진건 <sjg729729@gmail.com>
2024-09-13 02:53:54 +0900

    feat: UI init

--- components/plan/PlanStartModal.tsx
+++ components/plan/PlanStartModal.tsx
@@ -1,11 +1,25 @@
+'use client'
+
 import React, { ReactNode } from 'react'
 
 interface PlanStartModalProps {}
 
 const PlanStartModal = ({}: PlanStartModalProps): ReactNode => {
   return (
-    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
-      <div className='flex h-1/2 w-1/2 flex-col rounded bg-white'>PlanStartModal</div>
+    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-40'>
+      <div className='flex h-2/3 w-2/3 flex-col rounded-[30px] border-2 border-black bg-white'>
+        <div className='flex h-[15%] w-full rounded-t-[30px] border-b-2 border-black bg-amber-300'>
+          <div className='flex grow items-center justify-center border-r-2 border-black text-3xl font-semibold'>
+            여행 지역
+          </div>
+          <div className='flex grow items-center justify-center text-3xl font-semibold'>여행기간</div>
+        </div>
+        <div className='flex h-full w-full flex-col items-center justify-center'>
+          <p>어디로 떠나시나요?</p>
+          <p>* 지역을 이동하는 경우, 첫 방문지로 설정해주세요.</p>
+          <div>검색창</div>
+        </div>
+      </div>
     </div>
   )
 }
