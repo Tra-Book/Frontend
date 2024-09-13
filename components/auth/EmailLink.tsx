b76d52cda605ff65090705f99e78924b0754c141
Ji__ho <cale.jiho@gmail.com>
2024-09-13 21:08:57 +0900

    Style: login minor style change

--- components/auth/EmailLink.tsx
+++ components/auth/EmailLink.tsx
@@ -10,7 +10,7 @@ interface EmailLinkProps {
 
 const EmailLink = ({ className }: EmailLinkProps): ReactNode => {
   return (
-    <div className={cn('flex items-center justify-center gap-3 ssm:flex-col', className)}>
+    <div className={cn('flex flex-col items-center justify-center gap-3 ssm:flex-row', className)}>
       <p className='text-tbGray'>계정이 없으신가요?</p>
       <Link className='text-black underline hover:text-tbBlue' href={ROUTES.AUTH.SIGNUP.url}>
         이메일로 회원가입
