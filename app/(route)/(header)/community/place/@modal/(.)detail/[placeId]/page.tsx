4136a679c412ddd026aae96ab375113e5adf6998
신진건 <sjg729729@gmail.com>
2024-09-28 16:49:16 +0900

    Chore: modal setting

--- app/(route)/(header)/community/place/@modal/(.)detail/[placeId]/page.tsx
+++ app/(route)/(header)/community/place/@modal/(.)detail/[placeId]/page.tsx
@@ -2,13 +2,15 @@
 
 import React, { ReactNode } from 'react'
 
+import PlaceDetail from '@/components/community/place/PlaceDetail'
+
 interface CommunityPlaceDetailProps {}
 
 const CommunityPlaceDetail = ({}: CommunityPlaceDetailProps): ReactNode => {
   return (
     <div className='fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-40'>
       <div className='relative flex h-2/3 w-1/2 flex-col rounded-[30px] border-[1px] border-black bg-white'>
-        CommunityPlaceDetail
+        <PlaceDetail />
       </div>
     </div>
   )
---
+++ components/community/place/PlaceDetail.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React, { ReactNode } from 'react'
+
+import { CommunityPlace } from './placeType'
+
+interface PlaceDetailProps {
+  place: CommunityPlace
+}
+
+const PlaceDetail = ({}: PlaceDetailProps): ReactNode => {
+  return <div>PlaceDetail</div>
+}
+
+export default PlaceDetail
