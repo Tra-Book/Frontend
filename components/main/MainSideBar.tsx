fcee8950c71013567fb4d013816252b3962aab33
Ji__ho <cale.jiho@gmail.com>
2024-09-30 23:45:58 +0900

    Style: Main Page Sidebar

--- components/main/MainSideBar.tsx
+++ components/main/MainSideBar.tsx
@@ -35,63 +35,72 @@ const MainSideBar = ({}: SideBarProps): ReactNode => {
           {session.data?.status_message || NO_USER_DESCRIPTION}
         </div>
       </div>
-      {/* 링크 */}
-      <div className='flex w-3/4 flex-col items-start justify-start gap-8 py-10 text-xl font-medium 2xl:text-2xl'>
-        <Link href={ROUTES.MAIN.MY_PLAN.url} className='flex items-center justify-center gap-4'>
+
+      <div className='grid grid-cols-[40px_1fr] grid-rows-[20px_repeat(2,_minmax(80px,_auto))] items-start gap-x-2 gap-y-8 py-10 text-xl font-medium 2xl:text-2xl'>
+        {/* First Row - "내 계획" */}
+        <div className='flex items-center justify-center'>
           <LucideIcon name='Plane' size={26} strokeWidth={1.5} />
+        </div>
+        <Link href={ROUTES.MAIN.MY_PLAN.url} className='flex items-center gap-4'>
           <span className={cn('hover:text-tbRed', pathname === '/main' && 'font-semibold text-tbRed')}>내 계획</span>
         </Link>
 
-        <div className='flex w-full items-start justify-start gap-4'>
+        {/* Second Row - "보관함" */}
+        <div className='flex items-center justify-center'>
           <LucideIcon name='Bookmark' size={26} strokeWidth={1.5} />
-          <div className='flex flex-col items-start justify-start gap-2'>
-            <Link href={ROUTES.MAIN.STORE_PLAN.url}>보관함</Link>
-            <Link
-              href={ROUTES.MAIN.STORE_PLAN.url}
-              className={cn(
-                'text-base hover:text-tbRed 2xl:text-lg',
-                pathname.includes('store_plan') && 'font-semibold text-tbRed',
-              )}
-            >
-              여행 계획
-            </Link>
-            <Link
-              href={ROUTES.MAIN.STORE_PLACE.url}
-              className={cn(
-                'text-base hover:text-tbRed 2xl:text-lg',
-                pathname.includes('store_place') && 'font-semibold text-tbRed',
-              )}
-            >
-              여행지
-            </Link>
-          </div>
+        </div>
+        <div className='flex flex-col items-start justify-start gap-2'>
+          <Link href={ROUTES.MAIN.STORE_PLAN.url} className='text-xl'>
+            보관함
+          </Link>
+          <Link
+            href={ROUTES.MAIN.STORE_PLAN.url}
+            className={cn(
+              'text-base hover:text-tbRed 2xl:text-lg',
+              pathname.includes('store_plan') && 'font-semibold text-tbRed',
+            )}
+          >
+            여행 계획
+          </Link>
+          <Link
+            href={ROUTES.MAIN.STORE_PLACE.url}
+            className={cn(
+              'text-base hover:text-tbRed 2xl:text-lg',
+              pathname.includes('store_place') && 'font-semibold text-tbRed',
+            )}
+          >
+            여행지
+          </Link>
         </div>
 
-        <div className='flex w-full items-start justify-start gap-4'>
+        {/* Third Row - "내 정보" */}
+        <div className='flex items-center justify-center'>
           <LucideIcon name='Settings' size={26} strokeWidth={1.5} />
-          <div className='flex flex-col items-start justify-start gap-2'>
-            <Link href={ROUTES.MAIN.INFO.url}>내 정보</Link>
+        </div>
+        <div className='flex flex-col items-start justify-start gap-2'>
+          <Link href={ROUTES.MAIN.INFO.url} className='text-xl'>
+            내 정보
+          </Link>
+          <Link
+            href={ROUTES.MAIN.INFO.url}
+            className={cn(
+              'text-base hover:text-tbRed 2xl:text-lg',
+              pathname.includes('info') && 'font-semibold text-tbRed',
+            )}
+          >
+            프로필 수정
+          </Link>
+          {session.data?.provider === 'credentials' && (
             <Link
-              href={ROUTES.MAIN.INFO.url}
+              href={ROUTES.MAIN.CHANGE_PASSWORD.url}
               className={cn(
                 'text-base hover:text-tbRed 2xl:text-lg',
-                pathname.includes('info') && 'font-semibold text-tbRed',
+                pathname.includes('password') && 'font-semibold text-tbRed',
               )}
             >
-              프로필 수정
+              비밀번호 변경
             </Link>
-            {session.data?.provider === 'credentials' && (
-              <Link
-                href={ROUTES.MAIN.CHANGE_PASSWORD.url}
-                className={cn(
-                  'text-base hover:text-tbRed 2xl:text-lg',
-                  pathname.includes('password') && 'font-semibold text-tbRed',
-                )}
-              >
-                비밀번호 변경
-              </Link>
-            )}
-          </div>
+          )}
         </div>
       </div>
     </div>
