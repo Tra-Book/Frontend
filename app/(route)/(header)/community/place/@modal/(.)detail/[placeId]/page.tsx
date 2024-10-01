7d5ec7b20a039b7f18d1de1a30b5cdf8ab61c710
신진건 <sjg729729@gmail.com>
2024-10-02 02:53:27 +0900

    fix : z-index

--- app/(route)/(header)/community/place/@modal/(.)detail/[placeId]/page.tsx
+++ app/(route)/(header)/community/place/@modal/(.)detail/[placeId]/page.tsx
@@ -30,8 +30,8 @@ const CommunityPlaceDetail = async ({ params }: CommunityPlaceDetailProps): Prom
   const detailPlace: DetailPlace = await getPlace(params.placeId)
 
   return (
-    <div className='fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-40'>
-      <div className='relative flex h-[95%] w-[90%] min-w-[600px] flex-col rounded-[30px] border-[1px] border-black bg-white'>
+    <div className='fixed inset-0 z-40 flex items-center justify-center bg-black bg-opacity-40'>
+      <div className='relative z-50 flex h-[95%] w-[90%] min-w-[600px] flex-col rounded-[30px] border-[1px] border-black bg-white'>
         <PlaceDetail place={detailPlace} />
       </div>
     </div>
